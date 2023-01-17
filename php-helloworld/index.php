54d9a1481b21386cb5f39d114cf2435c7ee30eb8
Student User <student@workstation.lab.example.com>
2023-01-17 11:50:12 -0600

    changes done

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,3 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, DO180 My name is Prithvi " . PHP_VERSION . "\n";
-print "Hello, DO280 My name is pripatil " . PHP_VERSION . "\n";
 ?>
